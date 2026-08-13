e7fbecb98ad549306d02f54b8504394ff1fa13f4
Sepp Ungerer <sepp.programming11@outlook.com>
2026-08-13 16:36:29 +0200

    [FTC Code Utils] Auto-sync after build/deploy

---
+++ 2627/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/RobotHardware.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.robot;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Arm;
+import org.firstinspires.ftc.teamcode.robot.subsystems.Intake;
+
+/**
+ * Central access point for every robot subsystem.
+ *
+ * Generated and maintained by FTC Code Utils.
+ */
+public class RobotHardware {
+    private final LinearOpMode opMode;
+
+    public Arm arm;
+    public Intake intake;
+
+    public RobotHardware(LinearOpMode opMode) {
+        this.opMode = opMode;
+    }
+
+    public void init() {
+        arm = new Arm(opMode.hardwareMap);
+        intake = new Intake(opMode.hardwareMap);
+    }
+
+    public void updateAll() {
+        arm.update();
+        intake.update();
+    }
+}
