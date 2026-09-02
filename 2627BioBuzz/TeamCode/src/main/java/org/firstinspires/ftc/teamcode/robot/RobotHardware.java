217c2bc0d4cb6ce1c81bd6561c944fc8310f2ae5
Sepp Ungerer <sepp.programming11@outlook.com>
2026-09-02 07:45:39 +0200

    [FTC Code Utils] Manual Git sync

---
+++ 2627BioBuzz/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/RobotHardware.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.robot;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Central access point for every robot subsystem.
+ *
+ * Generated and maintained by FTC Code Utils.
+ */
+public class RobotHardware {
+    private final LinearOpMode opMode;
+
+    public RobotHardware(LinearOpMode opMode) {
+        this.opMode = opMode;
+    }
+
+    public void init() {
+        // Add subsystem classes under robot/subsystems to populate this automatically.
+    }
+
+    public void updateAll() {
+        // Subsystems with public void update() are called here automatically.
+    }
+}
